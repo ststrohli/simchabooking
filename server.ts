f572e3a6a591d77adebc50b53aa20f9c9d1d8a3c
ststrohli <ststrohli@gmail.com>
2026-04-30 00:21:10 -0400

    Update server.ts

--- server.ts
+++ server.ts
@@ -1561,3 +1561,8 @@ async function runDailyCheckIn() {
 }
 
 startServer();
+const port = Number(process.env.PORT) || 8080;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is listening on port ${port}`);
+});
